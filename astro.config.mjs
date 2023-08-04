4e56eb246e4416ca68ff6b77b4da2dbb111c6208
Damian Esteban <damian.esteban@gmail.com>
2023-08-03 22:53:37 -0400

    Add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://blog.damianesteban.dev";
+const LIVE_URL = "https://blog2.damianesteban.dev";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -26,17 +26,20 @@ let BASE_URL = LOCALHOST_URL;
 if (isBuild) {
   BASE_URL = LIVE_URL;
 }
-
 import serviceWorker from "astrojs-service-worker";
 
+// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT },
+  server: {
+    port: SERVER_PORT
+  },
   site: BASE_URL,
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    serviceWorker(),
-  ],
+  integrations: [sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), serviceWorker(), robotsTxt()]
 });
